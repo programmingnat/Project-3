b62d77dec35dd250bb0885d607a2d963ff7216c6
Noah Driscoll <noahdriscoll@gmail.com>
2016-03-07 18:09:00 -0500

    Second Commit: getBitMapFromURL method

--- justthejist/app/src/main/java/com/imaginat/justhejist/jist/Notifications/MaratNotifications.java
+++ justthejist/app/src/main/java/com/imaginat/justhejist/jist/Notifications/MaratNotifications.java
@@ -1,8 +1,17 @@
 package com.imaginat.justhejist.jist.Notifications;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 
+import com.imaginat.justhejist.jist.MainActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -11,9 +20,9 @@ import java.net.URL;
 public class MaratNotifications {
     private Activity mActivity = null;
     private String nameOfArticle;
-    private URL mUrl;
+    private String mUrl;
 
-    public MaratNotifications(Activity a, String articleName, URL url){
+    public MaratNotifications(Activity a, String articleName, String url){
         mActivity = a;
         nameOfArticle = articleName;
         mUrl = url;
@@ -23,5 +32,30 @@ public class MaratNotifications {
         builder.setSmallIcon(android.R.drawable.ic_lock_idle_lock);
         builder.setContentTitle("New Article: " + nameOfArticle);
         builder.setContentText("Check Out This New Interesting Story");
+
+        Intent notificationIntent = new Intent(mActivity,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(mActivity,(int)System.currentTimeMillis(),notificationIntent,0);
+
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+
+        NotificationCompat.BigPictureStyle bigPic = new NotificationCompat.BigPictureStyle().bigPicture(getBitmapFromURL(mUrl));
+
+
+    }
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
     }
 }
