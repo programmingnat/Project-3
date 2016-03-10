2e8aba80c49370cbbe5f192594e14d15ee842fea
Boloutare Doubeni <tarecd@gmail.com>
2016-03-10 15:39:41 -0500

    Parsed story

--- justthejist/app/src/main/java/com/imaginat/justhejist/jist/Activities/StoryActivity.java
+++ justthejist/app/src/main/java/com/imaginat/justhejist/jist/Activities/StoryActivity.java
@@ -1,15 +1,23 @@
 package com.imaginat.justhejist.jist.Activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.imaginat.justhejist.jist.R;
+import com.imaginat.justhejist.jist.api.nyt.WebScraper;
+
+import java.io.IOException;
 
 public class StoryActivity extends AppCompatActivity {
+
+  private String mUrl;
+  private TextView mTextView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,34 @@ public class StoryActivity extends AppCompatActivity {
     });
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    // TODO(boloutaredoubeni):
+    mUrl = getIntent().getExtras().getString("URL");
+
+    mTextView = (TextView)findViewById(R.id.story_txt);
+    // TODO(boloutaredoubeni): pull in the story object and use the members
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    new HTMLParserTask().execute(mUrl);
+  }
+
+  private class HTMLParserTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+      String url = params[0];
+      try {
+        return WebScraper.parseHTML(url);
+      } catch (IOException e) {
+        return e.getMessage();
+      }
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+      super.onPostExecute(s);
+      mTextView.setText(s);
+    }
   }
 }
--- justthejist/app/src/main/java/com/imaginat/justhejist/jist/api/nyt/WebScraper.java
+++ justthejist/app/src/main/java/com/imaginat/justhejist/jist/api/nyt/WebScraper.java
@@ -1,7 +1,16 @@
 package com.imaginat.justhejist.jist.api.nyt;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Copyright 2016 Boloutare Doubeni
  */
 public class WebScraper {
+
+  public static String parseHTML(String url) throws IOException {
+   return Jsoup.parse(new URL(url), 5000).body().getElementsByClass("article-body").text();
+  }
 }
