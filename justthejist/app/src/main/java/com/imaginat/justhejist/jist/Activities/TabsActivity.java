ddc2971395fb054309ac9bc8344a31de29f9194d
Noah Driscoll <noahdriscoll@gmail.com>
2016-03-10 09:36:50 -0500

    Eleventh Commit: Colored Tabs

--- justthejist/app/src/main/java/com/imaginat/justhejist/jist/Activities/TabsActivity.java
+++ justthejist/app/src/main/java/com/imaginat/justhejist/jist/Activities/TabsActivity.java
@@ -1,5 +1,7 @@
 package com.imaginat.justhejist.jist.Activities;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
@@ -8,35 +10,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.imaginat.justhejist.jist.R;
 import com.imaginat.justhejist.jist.tabs.MyPageAdapter;
 
 
 public class TabsActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+    Window mWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Tab 1");
+        mToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkerFrag1));
+//        mToolbar.setTitle("Tab 1");
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mWindow = this.getWindow();
+        mWindow.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDarkerFrag1));
+
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkerFrag1));
+
+
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final MyPageAdapter adapter = new MyPageAdapter
                 (getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+//        tabLayout.getTabAt(viewPager.getCurrentItem()).getText();
+
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+
+                switch(viewPager.getCurrentItem()) {
+                    case 0:
+                        mToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkFrag1));
+                        mToolbar.setTitle("Tab 1");
+                        tabLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkFrag1));
+                        mWindow.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDarkerFrag1));
+                        break;
+                    case 1:
+                        mToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkFrag2));
+                        mToolbar.setTitle("Tab 2");
+                        tabLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkFrag2));
+                        mWindow.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDarkerFrag2));
+                        break;
+                    case 2:
+                        mToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkFrag3));
+                        mToolbar.setTitle("Tab 3");
+                        tabLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDarkFrag3));
+                        mWindow.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDarkerFrag3));
+                        break;
+                    default:
+                }
             }
 
             @Override
